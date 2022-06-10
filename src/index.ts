35896e7e26d98dfcaaa292b700c18342c15caa3b
giuxtaposition <yg97.cs@gmail.com>
2022-06-10 12:14:05 +0200

    added config utils

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import dotenv from "dotenv"
 import express, { Express, Request, Response } from "express"
-
-dotenv.config()
+import { PORT } from "./utils/config"
 
 const app: Express = express()
-const port = process.env.PORT
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server")
 })
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
 })
